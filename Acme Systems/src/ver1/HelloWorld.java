84885d88fca29d101548cd162561b3c1fcc926ad
Greg Bohnhoff <gbohnhoff@gmail.com>
2019-09-05 11:00:05 -0400

    Added whatFor method

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -12,6 +12,11 @@ public class HelloWorld {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
+	
+	public static void whatFor(String msg) {
+		System.out.printf(msg, " stringArg, what for?");
+	}
+
 
 
 }
